bc592669778bb56b227a9fcf8855ddb0a515ed9d
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-02-18 18:48:33 -0800

    Reconnect stream if no activity for a while

--- src/actions/stream.js
+++ src/actions/stream.js
@@ -1,127 +1,89 @@
 'use strict';
 
-var dispatcher = require('../app_dispatcher');
-var constants = require('../constants');
-var syncActions = require('./sync');
+import _ from 'lodash';
+import dispatcher from '../app_dispatcher';
+import constants from '../constants';
+import syncActions from './sync';
 
-var debug = false;
-var source = null;
-var _authToken = null;
+// maximum time we can go without receiving anything from the server
+const MAX_INACTIVITY_TIME = 60000;
 
-/*
-EventSource close event doesn't fire when computer put into sleep mode, ESC is
-pressed during loading or when calling window.stop().
-https://code.google.com/p/chromium/issues/detail?id=225654
+let source = null;
+let _authToken = null;
 
-STR: Stream events, call window.stop()
+// Called on each interaction with the server
+// So when debounce is done we exceeded MAX_INACTIVITY_TIME.
+// Done because the error event listener on EventSource cannot be trusted.
+let scheduleHealthCheck = _.debounce(function() {
+  start(_authToken);
+}, MAX_INACTIVITY_TIME);
 
-Observe that source.readyState === EventSource.CLOSED and it is not trying to
-re-establish a connection.
-*/
-
-var _lastError = null;
-var _recoveryProtection = null;
-
-var CHROME_RECONNECT_INTERVAL = 3000;
-var RECOVERY_CHECK_INTERVAL = 1000;
-
-var startRecoveryProtection = function() {
-  var checkState = function() {
-    if (debug) console.log("check state, ", source.readyState, _lastError);
-
-    if (source.readyState === EventSource.CLOSED) {
-      var now = (new Date()).getTime();
-
-      if (_lastError === null || now - _lastError > CHROME_RECONNECT_INTERVAL) {
-        if (debug) console.log("fixing state, now:", now);
-        streamActions.start(_authToken);
-      }
-    }
-  };
-
-  _recoveryProtection = setInterval(checkState, RECOVERY_CHECK_INTERVAL);
-};
-
-var stopStream = function() {
+let stopStream = function() {
   source.close();
   source = null;
   _authToken = null;
-  _lastError = null;
-
-  clearInterval(_recoveryProtection);
-  _recoveryProtection = null;
+  scheduleHealthCheck.cancel();
 };
 
-var streamActions = {
-  isSupported() {
-    return !!EventSource;
-  },
+export function isSupported() {
+  return 'EventSource' in window;
+}
 
-  start(authToken) {
-    if (source !== null) {
-      if (source.readyState === EventSource.OPEN) {
-        return;
-      } else {
-        stopStream();
-      }
-    }
-
-    var url = '/api/stream';
-
-    if (authToken) {
-      url += '?api_password=' + authToken;
-    }
-
-    source = new EventSource(url);
-    _authToken = authToken;
-    startRecoveryProtection();
-
-    source.addEventListener('message', function(ev) {
-      dispatcher.dispatch({
-        actionType: constants.ACTION_REMOTE_EVENT_RECEIVED,
-        event: JSON.parse(ev.data),
-      });
-    }, false);
-
-    source.addEventListener('open', function() {
-      dispatcher.dispatch({
-        actionType: constants.ACTION_STREAM_START,
-      });
-
-      // We are streaming, stop synchronizing
-      syncActions.stopSync();
-      // make sure that we have the latest info when we start listening.
-      syncActions.fetchAll();
-    }, false);
-
-    source.addEventListener('error', function(ev) {
-      _lastError = (new Date()).getTime();
-
-      if (source.readyState == EventSource.CLOSED) {
-        streamActions.stop();
-      } else {
-        dispatcher.dispatch({
-          actionType: constants.ACTION_STREAM_ERROR,
-        });
-      }
-    }, false);
-
-  },
-
-  stop() {
+export function start(authToken) {
+  if (source !== null) {
     stopStream();
+  }
+
+  let url = '/api/stream';
+
+  if (authToken) {
+    url += '?api_password=' + authToken;
+  }
+
+  source = new EventSource(url);
+  _authToken = authToken;
+
+  source.addEventListener('open', function() {
+    scheduleHealthCheck();
 
     dispatcher.dispatch({
-      actionType: constants.ACTION_STREAM_STOP,
+      actionType: constants.ACTION_STREAM_START,
     });
 
-    syncActions.sync();
-  },
+    // We are streaming, fetch latest info but stop streaming
+    syncActions.stopSync();
+    syncActions.fetchAll();
+  }, false);
 
-  debug() {
-    window._stream_source = source;
-    debug = true;
-  },
-};
+  source.addEventListener('message', function(ev) {
+    scheduleHealthCheck();
 
-module.exports = streamActions;
+    if (ev.data === 'ping') {
+      return;
+    }
+
+    dispatcher.dispatch({
+      actionType: constants.ACTION_REMOTE_EVENT_RECEIVED,
+      event: JSON.parse(ev.data),
+    });
+  }, false);
+
+  source.addEventListener('error', function(ev) {
+    if (source.readyState !== EventSource.CLOSED) {
+      dispatcher.dispatch({
+        actionType: constants.ACTION_STREAM_ERROR,
+      });
+    }
+  }, false);
+
+}
+
+export function stop() {
+  stopStream();
+
+  dispatcher.dispatch({
+    actionType: constants.ACTION_STREAM_STOP,
+  });
+
+  syncActions.sync();
+}

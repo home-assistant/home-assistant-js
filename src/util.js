03885542c46c08f22250e8c52eb49490054fce0c
McSwindler <wilingua@gmail.com>
2015-03-16 18:15:40 -0500

    changed parseDateTime to use Date.parse()

--- src/util.js
+++ src/util.js
@@ -1,9 +1,5 @@
 'use strict';
 
 export function parseDateTime(datetime) {
-  let [time, date] = datetime.split(" ");
-  let [hour, minute, second] = time.split(":");
-  let [day, month, year] = date.split("-");
-
-  return new Date(Date.UTC(year, parseInt(month)-1, day, hour, minute, second));
+  return new Date(Date.parse(datetime));
 }

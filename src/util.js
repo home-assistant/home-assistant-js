958329585ae4731fd79f325d1c1997d39038e4dc
McSwindler <wilingua@gmail.com>
2015-03-17 06:25:07 +0000

    set date to parse UTC

--- src/util.js
+++ src/util.js
@@ -1,5 +1,7 @@
 'use strict';
 
 export function parseDateTime(datetime) {
+  if(datetime.slice(-1) != 'Z')
+	datetime += 'Z' //make sure it's UTC timezone
   return new Date(Date.parse(datetime));
 }

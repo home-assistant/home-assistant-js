124b5df8ffb08bd7db22912865dba80845815e5d
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-04-28 22:28:27 -0700

    Treat all datetime strings from HA API as UTC

--- src/util.js
+++ src/util.js
@@ -3,5 +3,5 @@ export function parseDateTime(datetime) {
   let [hour, minute, second] = time.split(":");
   let [day, month, year] = date.split("-");
 
-  return new Date(year, parseInt(month)-1, day, hour, minute, second);
+  return new Date(Date.UTC(year, parseInt(month)-1, day, hour, minute, second));
 }

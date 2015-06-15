cec99925399b1c8e8cea15bbbba5d873522ccbd6
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-06-15 00:18:23 -0700

    Logbook: allow fetching specific days

--- src/actions/logbook.js
+++ src/actions/logbook.js
@@ -2,13 +2,21 @@ import callApi from '../call_api';
 import dispatcher from '../app_dispatcher';
 import constants from '../constants';
 
-function newLogbookEntries(logbookEntries) {
+function newLogbookEntries(date, logbookEntries) {
   dispatcher.dispatch({
     actionType: constants.ACTION_NEW_LOGBOOK,
+    date: date,
     logbookEntries: logbookEntries,
   });
 }
 
-export function fetch() {
-  callApi('GET', 'logbook').then(newLogbookEntries);
+export function fetch(date=null) {
+  let url = 'logbook'
+
+  if (date != null) {
+    url += `/${date}`
+  }
+
+  callApi('GET', url).then(
+    logbookEntries => newLogbookEntries(date, logbookEntries));
 }
--- src/stores/logbook.js
+++ src/stores/logbook.js
@@ -3,18 +3,19 @@ import { List } from 'immutable';
 import dispatcher from '../app_dispatcher';
 import constants from '../constants';
 import Store from '../stores/store';
-import LogbookEntry from '../models/logbook_entry'; 
+import LogbookEntry from '../models/logbook_entry';
 
 // Consider data stale if not fetched in last minute
 const STALE_TIME = 60000;
 
 let _lastUpdated = null;
+let _date = null;
 let _logbook = new List();
 
 class HumanLogStore extends Store {
 
-  isStale() {
-    return _lastUpdated === null ||
+  shouldFetch(date=null) {
+    return date != _date || _lastUpdated === null ||
            (new Date()).getTime() - _lastUpdated.getTime() > STALE_TIME;
   }
 
@@ -28,6 +29,7 @@ const INSTANCE = new HumanLogStore();
 INSTANCE.dispatchToken = dispatcher.register(function(payload) {
   switch(payload.actionType) {
     case constants.ACTION_NEW_LOGBOOK:
+      _date = payload.date;
       _logbook = new List(
         payload.logbookEntries.map(
           entry => LogbookEntry.fromJSON(entry)));
@@ -38,6 +40,7 @@ INSTANCE.dispatchToken = dispatcher.register(function(payload) {
       break;
 
     case constants.ACTION_LOG_OUT:
+      _date = null;
       _lastUpdated = null;
       _logbook = new List();
       INSTANCE.emitChange();

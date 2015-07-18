2819bd78ce5cc0048bfa8c42a2073a20783b75f9
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-07-17 23:24:58 -0700

    Close dialogs when navigating history

--- src/modules/navigation/url-sync.js
+++ src/modules/navigation/url-sync.js
@@ -1,15 +1,20 @@
 /* Keeps the URL in sync with the navigator store */
 import Flux from '../../flux';
+import {
+  getters as moreInfoGetters,
+  actions as moreInfoActions
+} from '../more-info';
 import { activePane, activeFilter, activePage } from './getters';
 import { navigate } from './actions';
 import paneFilterToPage from './pane-filter-to-page';
 import pageToPaneFilter from './page-to-pane-filter';
 
+const isSupported = history.pushState && !__DEMO__;
 const title = 'Home Assistant';
 
-let unwatchObserver;
-
-const isSupported = history.pushState && !__DEMO__;
+let ignoreNextDeselectEntity = false;
+let unwatchNavigationObserver;
+let unwatchMoreInfoObserver;
 
 function initialSync() {
   let pane, filter, url;
@@ -30,7 +35,13 @@ function initialSync() {
 
 function popstateChangeListener(ev) {
   const {pane, filter} = ev.state;
-  navigate(pane, filter);
+
+  if (Flux.evaluate(moreInfoGetters.hasCurrentEntityId)) {
+    ignoreNextDeselectEntity = true;
+    moreInfoActions.deselectEntity();
+  } else {
+    navigate(pane, filter);
+  }
 };
 
 export function startSync() {
@@ -41,13 +52,26 @@ export function startSync() {
   initialSync();
 
   // keep url in sync with state
-  unwatchObserver = Flux.observe(activePage, function activePageChanged(page) {
+  unwatchNavigationObserver = Flux.observe(activePage, (page) => {
     const state = pageToPaneFilter(page);
     if (!(state.pane === history.state.pane &&
           state.filter == history.state.filter)) {
-      history.pushState(state, title, `/${page}`);
+      history.pushState(state, page, `/${page}`);
     }
   });
+
+  unwatchMoreInfoObserver = Flux.observe(
+    moreInfoGetters.hasCurrentEntityId,
+    (moreInfoEntitySelected) => {
+      if (moreInfoEntitySelected) {
+        history.pushState(history.state, title, location.pathname);
+      } else if (ignoreNextDeselectEntity) {
+        ignoreNextDeselectEntity = false;
+      } else {
+        history.back();
+      }
+    }
+  );
 
   // keep state in sync when url changes via forward/back buttons
   window.addEventListener('popstate', popstateChangeListener);
@@ -58,8 +82,9 @@ export function stopSync() {
     return;
   }
 
-  if (unwatchObserver) {
-    unwatchObserver();
+  if (unwatchNavigationObserver) {
+    unwatchNavigationObserver();
+    unwatchMoreInfoObserver();
   }
 
   window.removeEventListener('popstate', popstateChangeListener);

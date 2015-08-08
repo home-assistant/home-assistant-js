26f7eae99e1243691dc2d76e2c7c3dbf3f1bf0d0
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-08-07 19:01:41 -0700

    Update sync to support multiple reactors

--- src/modules/sync/actions.js
+++ src/modules/sync/actions.js
@@ -9,35 +9,55 @@ import { actions as eventActions } from '../event';
 import { actions as configActions } from '../config';
 
 const SYNC_INTERVAL = 30000;
+const SCHEDULED_SYNCS = {};
+
+function isSyncing(reactor) {
+  return reactor.evaluate(getters.isSyncScheduled);
+}
+
+function scheduleSync(reactor) {
+  if (!isSyncing(reactor)) {
+    return;
+  }
+  if (!(reactor.hassId in SCHEDULED_SYNCS)) {
+    /* eslint-disable no-use-before-define */
+    SCHEDULED_SYNCS[reactor.hassId] = debounce(fetchAll.bind(null, reactor), SYNC_INTERVAL);
+    /* eslint-enable no-use-before-define */
+  }
+
+  SCHEDULED_SYNCS[reactor.hassId]();
+}
+
+function unscheduleSync(reactor) {
+  const sync = SCHEDULED_SYNCS[reactor.hassId];
+
+  if (sync) {
+    sync.cancel();
+  }
+}
 
 export function fetchAll(reactor) {
-  const isSyncScheduled = reactor.evaluate(getters.isSyncScheduled);
-
   reactor.dispatch(actionTypes.API_FETCH_ALL_START, {});
 
   return callApi(reactor, 'GET', 'bootstrap').then(data => {
-    entityActions.replaceData(reactor, data.states);
-    serviceActions.replaceData(reactor, data.services);
-    eventActions.replaceData(reactor, data.events);
-    configActions.configLoaded(reactor, data.config);
+    reactor.batch(() => {
+      entityActions.replaceData(reactor, data.states);
+      serviceActions.replaceData(reactor, data.services);
+      eventActions.replaceData(reactor, data.events);
+      configActions.configLoaded(reactor, data.config);
 
-    reactor.dispatch(actionTypes.API_FETCH_ALL_SUCCESS, {});
+      reactor.dispatch(actionTypes.API_FETCH_ALL_SUCCESS, {});
+    });
 
-    if (isSyncScheduled) {
-      scheduleSync(reactor);
-    }
+    scheduleSync(reactor);
   }, message => {
     reactor.dispatch(actionTypes.API_FETCH_ALL_FAIL, {message});
 
-    if (isSyncScheduled) {
-      scheduleSync(reactor);
-    }
+    scheduleSync(reactor);
 
     return Promise.reject(message);
   });
 }
-
-const scheduleSync = debounce(fetchAll, SYNC_INTERVAL);
 
 export function start(reactor, {skipInitialSync=false}={}) {
   reactor.dispatch(actionTypes.SYNC_SCHEDULED);
@@ -51,5 +71,5 @@ export function start(reactor, {skipInitialSync=false}={}) {
 
 export function stop(reactor) {
   reactor.dispatch(actionTypes.SYNC_SCHEDULE_CANCELLED);
-  scheduleSync.cancel();
+  unscheduleSync(reactor);
 }

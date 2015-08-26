d02264b0f96c31288ca3d2c1884f833986cee298
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-08-26 08:37:16 -0700

    Expand group filters out not found states

--- src/util/expand-group.js
+++ src/util/expand-group.js
@@ -1,5 +1,8 @@
-// Returns array with group states
+// Returns Immutable list with group states.
+// Entities that can't be found will be skipped.
+import { toImmutable } from 'nuclear-js';
+
 export default function expandGroup(groupState, entityMap) {
-  return groupState.attributes.entity_id.map(
-    entityId => entityMap.get(entityId));
+  return toImmutable(groupState.attributes.entity_id.map(
+    entityId => entityMap.get(entityId)).filter(ent => !!ent));
 }

1c29568abc9af18627cd70cab2262b32786cd63d
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-11-29 22:47:49 -0800

    Add support for locks in canToggleDomain

--- src/modules/service/can-toggle-domain.js
+++ src/modules/service/can-toggle-domain.js
@@ -1,4 +1,5 @@
 export default function canToggleDomain(domain, servicesMap) {
+  if (domain === 'lock') return true;
   const serviceDomain = servicesMap.get(domain);
   return !!serviceDomain && serviceDomain.services.has('turn_on');
 }

5cd422866e1545ba9d0801abe3c5332077fcb042
Eric Rolf <erolf@cardinalsolutions.com>
2015-12-20 14:10:15 -0500

    Added support for garage door toggle domain.

--- src/modules/service/can-toggle-domain.js
+++ src/modules/service/can-toggle-domain.js
@@ -1,5 +1,6 @@
 export default function canToggleDomain(domain, servicesMap) {
   if (domain === 'lock') return true;
+  if (domain === 'garage_door') return true;
   const serviceDomain = servicesMap.get(domain);
   return !!serviceDomain && serviceDomain.services.has('turn_on');
 }

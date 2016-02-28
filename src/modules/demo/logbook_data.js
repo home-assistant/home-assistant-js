e2fe8b74d0b52296b03c82f8acfe5d69dcc36765
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-02-27 22:17:59 -0800

    Style Fix demo logbook data

--- src/modules/demo/logbook_data.js
+++ src/modules/demo/logbook_data.js
@@ -1,93 +1,93 @@
 export default [
-    {
-        "domain": "sun",
-        "entity_id": "sun.sun",
-        "message": "has risen",
-        "name": "sun",
-        "when": "06:08:47 24-04-2015"
-    },
-    {
-        "domain": "device_tracker",
-        "entity_id": "device_tracker.paulus",
-        "message": "left home",
-        "name": "Paulus",
-        "when": "08:54:47 24-04-2015"
-    },
-    {
-        "domain": "device_tracker",
-        "entity_id": "device_tracker.anne_therese",
-        "message": "left home",
-        "name": "Anne Therese",
-        "when": "09:08:47 24-04-2015"
-    },
-    {
-        "domain": "group",
-        "entity_id": "group.all_devices",
-        "message": "left home",
-        "name": "All devices",
-        "when": "09:08:47 24-04-2015"
-    },
-    {
-        "domain": "thermostat",
-        "entity_id": "thermostat.nest",
-        "message": "changed to 17 \u00b0C",
-        "name": "Nest",
-        "when": "09:08:47 24-04-2015"
-    },
-    {
-        "domain": "thermostat",
-        "entity_id": "thermostat.nest",
-        "message": "changed to 21 \u00b0C",
-        "name": "Nest",
-        "when": "16:00:47 24-04-2015"
-    },
-    {
-        "domain": "device_tracker",
-        "entity_id": "device_tracker.anne_therese",
-        "message": "came home",
-        "name": "Anne Therese",
-        "when": "16:24:47 24-04-2015"
-    },
-    {
-        "domain": "group",
-        "entity_id": "group.all_devices",
-        "message": "came home",
-        "name": "All devices",
-        "when": "16:24:47 24-04-2015"
-    },
-    {
-        "domain": "light",
-        "entity_id": "light.bowl",
-        "message": "turned on",
-        "name": "Bowl",
-        "when": "18:01:47 24-04-2015"
-    },
-    {
-        "domain": "light",
-        "entity_id": "light.ceiling",
-        "message": "turned on",
-        "name": "Ceiling",
-        "when": "18:16:47 24-04-2015"
-    },
-    {
-        "domain": "light",
-        "entity_id": "light.tv_back_light",
-        "message": "turned on",
-        "name": "TV Back Light",
-        "when": "18:31:47 24-04-2015"
-    },
-    {
-        "domain": "sun",
-        "entity_id": "sun.sun",
-        "message": "has set",
-        "name": "sun",
-        "when": "18:46:47 24-04-2015"
-    },
-    {
-        "domain": "media_player",
-        "entity_id": "media_player.living_room",
-        "message": "changed to Plex",
-        "name": "Media Player",
-        "when": "19:12:47 24-04-2015"
-    },
+  {
+    domain: 'sun',
+    entity_id: 'sun.sun',
+    message: 'has risen',
+    name: 'sun',
+    when: '06:08:47 24-04-2015',
+  },
+  {
+    domain: 'device_tracker',
+    entity_id: 'device_tracker.paulus',
+    message: 'left home',
+    name: 'Paulus',
+    when: '08:54:47 24-04-2015',
+  },
+  {
+    domain: 'device_tracker',
+    entity_id: 'device_tracker.anne_therese',
+    message: 'left home',
+    name: 'Anne Therese',
+    when: '09:08:47 24-04-2015',
+  },
+  {
+    domain: 'group',
+    entity_id: 'group.all_devices',
+    message: 'left home',
+    name: 'All devices',
+    when: '09:08:47 24-04-2015',
+  },
+  {
+    domain: 'thermostat',
+    entity_id: 'thermostat.nest',
+    message: 'changed to 17 \u00b0C',
+    name: 'Nest',
+    when: '09:08:47 24-04-2015',
+  },
+  {
+    domain: 'thermostat',
+    entity_id: 'thermostat.nest',
+    message: 'changed to 21 \u00b0C',
+    name: 'Nest',
+    when: '16:00:47 24-04-2015',
+  },
+  {
+    domain: 'device_tracker',
+    entity_id: 'device_tracker.anne_therese',
+    message: 'came home',
+    name: 'Anne Therese',
+    when: '16:24:47 24-04-2015',
+  },
+  {
+    domain: 'group',
+    entity_id: 'group.all_devices',
+    message: 'came home',
+    name: 'All devices',
+    when: '16:24:47 24-04-2015',
+  },
+  {
+    domain: 'light',
+    entity_id: 'light.bowl',
+    message: 'turned on',
+    name: 'Bowl',
+    when: '18:01:47 24-04-2015',
+  },
+  {
+    domain: 'light',
+    entity_id: 'light.ceiling',
+    message: 'turned on',
+    name: 'Ceiling',
+    when: '18:16:47 24-04-2015',
+  },
+  {
+    domain: 'light',
+    entity_id: 'light.tv_back_light',
+    message: 'turned on',
+    name: 'TV Back Light',
+    when: '18:31:47 24-04-2015',
+  },
+  {
+    domain: 'sun',
+    entity_id: 'sun.sun',
+    message: 'has set',
+    name: 'sun',
+    when: '18:46:47 24-04-2015',
+  },
+  {
+    domain: 'media_player',
+    entity_id: 'media_player.living_room',
+    message: 'changed to Plex',
+    name: 'Media Player',
+    when: '19:12:47 24-04-2015',
+  },
 ];

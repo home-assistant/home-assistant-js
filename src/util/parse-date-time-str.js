1ea38d86f8c4cac2b9e1780b875f1b33179e5e61
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-04-16 01:58:38 -0700

    Update datetime parsing for iso8601

--- src/util/parse-date-time-str.js
+++ src/util/parse-date-time-str.js
@@ -1,7 +1,3 @@
 export default function parseDateTime(datetime) {
-  const [time, date] = datetime.split(' ');
-  const [hour, minute, second] = time.split(':');
-  const [day, month, year] = date.split('-');
-
-  return new Date(Date.UTC(year, parseInt(month, 10) - 1, day, hour, minute, second));
+  return new Date(datetime);
 }

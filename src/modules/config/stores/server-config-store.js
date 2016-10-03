479681020ab5d9c42647eed254ceeeb56c31f36d
Justin Weberg <justin.weberg@gmail.com>
2016-10-03 07:29:54 -0500

    Revert "Update default config_dir to null"
    This reverts commit fa99315f91dfcad03dcd5778e2b1863b456880df.

--- src/modules/config/stores/server-config-store.js
+++ src/modules/config/stores/server-config-store.js
@@ -11,7 +11,7 @@ const INSTANCE = new Store({
       location_name: 'Home',
       temperature_unit: '°C',
       time_zone: 'UTC',
-      config_dir: null,
+      config_dir: '~/.homeassistant',
       serverVersion: 'unknown',
     });
   },
